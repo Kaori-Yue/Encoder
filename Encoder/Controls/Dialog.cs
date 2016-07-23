bf68942bfad22a4f56544be1c3103df04ec04ffc
CrossKnight SecondLast <forgetmenotforevertime@hotmail.com>
2016-07-23 23:32:44 +0700

    Update code & ui
    flyout video

---
+++ Encoder/Controls/Dialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encoder.Controls
+{
+    class Dialog
+    {
+        // Open
+        public string Openfile(string filter)
+        {
+            Microsoft.Win32.OpenFileDialog open = new Microsoft.Win32.OpenFileDialog();
+            open.Filter = filter;
+            Nullable<bool> result = open.ShowDialog();
+            if (result == true)
+            {
+                return open.FileName;
+            }
+            return null;
+        }
+
+        //Save
+        public string Savefile(string filter)
+        {
+            Microsoft.Win32.SaveFileDialog save = new Microsoft.Win32.SaveFileDialog();
+            save.Filter = filter;
+            Nullable<bool> result = save.ShowDialog();
+            if (result == true)
+            {
+                return save.FileName;
+            }
+            return null;
+        }
+    }
+}
--- Encoder/MainWindow.xaml.cs
+++ Encoder/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
-
+using System.Diagnostics;
 namespace Encoder
 {
     /// <summary>
@@ -24,5 +24,80 @@ namespace Encoder
         {
             InitializeComponent();
         }
+
+        // toggle Flyout
+        private void toggleFlyoutVideo(object sender, RoutedEventArgs e)
+        {
+            if (Setting_Video_Flyout.IsOpen == true)
+            { Setting_Video_Flyout.IsOpen = false; }
+            else
+            { Setting_Video_Flyout.IsOpen = true; }
+        }
+        //
+        private void toggleFlyoutAudio(object sender, RoutedEventArgs e)
+        {
+            if (Setting_Audio_Flyout.IsOpen == true)
+            { Setting_Audio_Flyout.IsOpen = false; }
+            else
+            { Setting_Audio_Flyout.IsOpen = true; }
+        }
+        //
+        private void toggleFlyoutPath(object sender, RoutedEventArgs e)
+        {
+            if (Setting_Path_Flyout.IsOpen == true)
+            { Setting_Path_Flyout.IsOpen = false; }
+            else
+            { Setting_Path_Flyout.IsOpen = true; }
+        }
+
+        //
+        // Dialog Open/Save file
+        private void _Openfile_Video(object sender, MouseButtonEventArgs e)
+        {
+            var obj = sender as TextBox;
+            if (obj != null)
+            {
+                Controls.Dialog file = new Controls.Dialog();
+                var filename = file.Openfile("Video file (*.mp4;*.mkv)|*.mp4;*.mkv|All file (*.*)|*.*");
+                //check select file
+                if (filename != null)
+                {
+                    obj.Text = filename;
+                }
+            }
+        }
+
+        // Subtitle
+        private void _Openfile_Ass(object sender, MouseButtonEventArgs e)
+        {
+            var obj = sender as TextBox;
+            if (obj != null)
+            {
+                Controls.Dialog file = new Controls.Dialog();
+                var filename = file.Openfile("Subtitle file (*.ass)|*.ass|All file (*.*)|*.*");
+                //check select file
+                if (filename != null)
+                {
+                    obj.Text = filename;
+                }
+            }
+        }
+
+        // Save file
+        private void _Savefile_Video(object sender, MouseButtonEventArgs e)
+        {
+            var obj = sender as TextBox;
+            if (obj != null)
+            {
+                Controls.Dialog file = new Controls.Dialog();
+                var filename = file.Savefile("MP4 file (*.mp4)|*.mp4|All file (*.*)|*.*");
+                //check
+                if (filename != null)
+                {
+                    obj.Text = filename;
+                }
+            }
+        }
+        //
     }
 }
