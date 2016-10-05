08543fd1cd44991f88e939006e8f27e52715f2c7
CrossKnight <forgetmenotforevertime@hotmail.com>
2016-10-06 03:06:23 +0700

    Update Theme

--- Encoder/Controls/Startup.cs
+++ Encoder/Controls/Startup.cs
@@ -11,14 +11,34 @@ namespace Encoder.Controls
         public void Main()
         {
             //
+            CurrentTheme();
         }
 
-        void Check()
+        void CheckFile()
         {
             if (File.Exists(""))
             {
                 //
             }
         }
+
+        //
+
+        void CurrentTheme()
+        {
+            Tuple<MahApps.Metro.AppTheme, MahApps.Metro.Accent> current = MahApps.Metro.ThemeManager.DetectAppStyle(System.Windows.Application.Current);
+            //System.Windows.MessageBox.Show(current.Item2.Name.ToString());
+            /*
+            List<MahApps.Metro.Accent> style = MahApps.Metro.ThemeManager.Accents.ToList();
+            Console.WriteLine();
+
+            Console.WriteLine("Log:");
+            Console.WriteLine(style[1].Name);
+            //System.Windows.MessageBox.Show(style[1].Name);
+            //Console.WriteLine(MahApps.Metro.ThemeManager.GetAccent(style[1]).Name.ToString());
+            //style.ForEach(Console.WriteLine);
+            */
+
+    }
     }
 }
---
+++ Encoder/Controls/Theme.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encoder.Controls
+{
+    public class Theme
+    {
+
+        private List<string> _Accents = new List<string>()
+        {
+            //
+        };
+
+
+        public Theme()
+        {
+            List<MahApps.Metro.Accent> style = MahApps.Metro.ThemeManager.Accents.ToList();
+            for (int i = 0; i < style.Count - 1; i++)
+            {
+                _Accents.Add(style[i].Name);
+            }
+            _Accents.Sort();
+
+        }
+
+
+        public List<string> Accents
+        {
+            get
+            {
+                return _Accents;
+            }
+            /*
+            set
+            {
+
+            }
+            */
+        }
+
+
+
+    }
+}
